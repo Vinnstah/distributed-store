7bf9c6ff58ea4f684d66f1ec526dd0ed9ecbbe3a
Viktor Jansson <v.l.jansson@gmail.com>
2024-04-17 20:03:05 +0200

    Add TcpListner to server and port argument

--- crates/distributed-server/src/main.rs
+++ crates/distributed-server/src/main.rs
@@ -1,5 +1,19 @@
+use std::env;
+use std::net::TcpListener;
 
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    
+    let port: u16 = args
+        .get(1)
+        .expect("Failed to get port argument")
+        .parse()
+        .expect("Failed to parse arg as u16");
 
-fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind(("127.0.0.1", port)).unwrap();
+
+    for stream in listener.incoming() {
+        println!("{:#?}", stream)
+    }
+    Ok(())
 }
